113b7eaecd2ec09e2a16e666812d6e624ff50681
Erisa Dervishi <erisa85d@gmail.com>
2015-04-16 18:17:41 -0400

    FileDiffer not finished

--- src/main/scala/Logger/FileDiffer.scala
+++ src/main/scala/Logger/FileDiffer.scala
@@ -3,6 +3,19 @@ package Logger
 /**
  * Created by gmgilmore on 4/16/15.
  */
-object FileDiffer {
 
+import difflib._
+import scala.collection.JavaConversions._
+
+object FileDiffer extends App{
+
+
+
+  def getDiffs(original: String, revised: String) = {
+    DiffUtils.diff(LogFileReader.getLines(original).toList, LogFileReader.getLines(revised).toList)
+  }
+
+  val res = getDiffs("/Users/erisa/Downloads/filediff/bad.scala", "/Users/erisa/Downloads/filediff/good.scala")
+
+  res.getDeltas.foreach(delta => println(delta.getType +  " original" + delta.getOriginal + " revised:" + delta.getRevised ))
 }
